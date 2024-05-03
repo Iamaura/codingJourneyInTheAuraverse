231824f44b643f9c0cc8fec1eb80f1252abc5bec
iamaura68 <92586879+Iamaura@users.noreply.github.com>
2024-05-03 18:04:51 -0400

    User_Append.py
    This personal project objective is to create a system where user input is stored into a database. This database acts as a social media platform where user's comments can be stored up to a certain amount per time period. I will be putting to practice what I've learn so far in python in order to create a system that's able to do the 8 listed steps in the beginning of the program.

---
+++ User_Append.py
@@ -0,0 +1,24 @@
+#    Initialize a List: Create a list named database to store user inputs.
+#    Input Loop: Set up a loop that continuously prompts the user for input.
+#    Input Limit Condition: Implement a condition inside the loop that checks if the length of database has reached 5. If true, exit the loop.
+#    Limit Reached Message: When the limit is reached, print a message informing the user that they have reached the maximum input limit.
+#    Timer Implementation: Look up how to use a timer in your programming language and set a timer for a specific duration.
+#    Wait for Timer: After reaching the input limit, wait for the timer to finish.
+#    Timer Completion Message: Once the timer ends, display a message letting the user know they can continue to input messages.
+#    Resume Input: After the timer message, prompt the user again to continue adding inputs to database.
+
+
+
+#       Initialize a List: Create a list named database to store user inputs.
+
+database = []
+
+user_message = input('Add a comment: ')
+#       Input Loop: Set up a loop that continuously prompts the user for input.
+
+while len(database) < 5: 
+    user_message = input('Add a comment: ')
+    database.append(user_message)
+    if len(database) == 5:
+        print("\tSorry your last comment was not posted.")
+        print("\tYou've reached the max amount of comments for now, please wait.")
